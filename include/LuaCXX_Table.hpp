2d2e59e6102cb1867b2f29a4aee7f79406da5bb6
BridgeDeck <littlefast2@gmail.com>
2025-07-13 15:21:09 +0800

    Now exclusively static library compilation. Also added constant strings for metamethod names for convenience.

--- include/LuaCXX_Table.hpp
+++ include/LuaCXX_Table.hpp
@@ -23,6 +23,29 @@ class Table: public Variant
     friend class Lua;
     friend class Variant;
 
+    public:
+    static constexpr const char* METAMETHOD_ADD = "__add";
+	static constexpr const char* METAMETHOD_SUBTRACT = "__sub";
+	static constexpr const char* METAMETHOD_MULTIPLY = "__mul";
+	static constexpr const char* METAMETHOD_DIVIDE = "__div";
+	static constexpr const char* METAMETHOD_NEGATION = "__unm";
+	static constexpr const char* METAMETHOD_MODULO = "__mod";
+	static constexpr const char* METAMETHOD_EXPONENT = "__pow";
+
+	static constexpr const char* METAMETHOD_EQUAL = "__eq";
+	static constexpr const char* METAMETHOD_LESSTHAN = "__lt";
+	static constexpr const char* METAMETHOD_LESSTHAN_EQUALTO = "__le";
+
+	static constexpr const char* METAMETHOD_CONCAT = "__concat";
+	static constexpr const char* METAMETHOD_LENGTH = "__len";
+
+	static constexpr const char* METAMETHOD_INDEX = "__index";
+	static constexpr const char* METAMETHOD_NEWINDEX = "__newindex";
+
+	static constexpr const char* METAMETHOD_CALL = "__call";
+	
+	static constexpr const char* METAMETHOD_TOSTRING = "__tostring";
+
 	protected:
 	Table(lua_State* lua, int stack_index);
 };
